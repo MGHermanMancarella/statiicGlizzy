91b9797cddb4c27ff73a01663fc28d1a8895642c
Michael <mherman315@gmail.com>
2023-08-09 14:04:37 -0700

    form:
    added form for location but having some issues on mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+"use strict";
+const locationForm = document.getElementById("locationForm");
+const glizzyResponse = document.getElementById("glizzyResponse");
+
 /*!
  * Start Bootstrap - Business Casual v7.0.9 (https://startbootstrap.com/theme/business-casual)
  * Copyright 2013-2023 Start Bootstrap
@@ -9,75 +13,99 @@
 //     listHoursArray[new Date().getDay()].classList.add(('today'));
 // })
 
-// Makes API call to get client's IP address
-async function getIpLocation() {
-  const res = await fetch("https://api.ipify.org?format=json");
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+// Makes API call to get client's IP address NOTE: The ip tracker was too inaccurate
+// async function getIpLocation() {
+//   const res = await fetch("https://api.ipify.org?format=json");
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error("Failed to fetch data");
+//   }
+//   return res.json();
+// }
 
-// Makes api call to get the weather from a specific ip address
-async function getWeather() {
-  let ipRes;
+/** Add event listener to location form
+ *
+ * form submits the query string that will be passed into the API call.
+ * That data can be anything location related, spaces included.
+ * - 99999
+ * - city name
+ *
+ */
+locationForm.addEventListener("submit", (event) => {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get the location query from the input field
+  const locationQuery = document.getElementById("locationQuery").value;
+
+  // Hide the form
+  document.getElementById("locationForm").style.display = "none";
+
+  // Send a GET request to the API
+  displayData(locationQuery);
+});
+
+/* Makes api call to get the weather from a specific ip address */
+async function getWeather(locationQuery) {
+  let res;
   try {
-    ipRes = await getIpLocation();
+    res = await fetch(
+      `https://api.weatherapi.com/v1/current.json?q=${locationQuery}`,
+      {
+        headers: {
+          key: "03684ae6e20549df9d4190834230208",
+        },
+      }
+    );
   } catch (error) {
-    // This will activate the closest `error.js` Error Boundary
-    return "Uh Oh! Something went wrong and we couldn't fetch data the correct  Try again";
+    alert();
+    locationForm.style.display = 'block';
+    document.getElementById("error").style.display = 'block'
+    throw error; 
   }
-  const res = await fetch(
-    `https://api.weatherapi.com/v1/current.json?q=${ipRes.ip}`,
-    {
-      headers: {
-        key: "03684ae6e20549df9d4190834230208",
-      },
-    }
-  );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
   return res.json();
 }
-window.onload = function () {
-  displayData();
-};
 
 // Displays data from async functions
-async function displayData() {
+async function displayData(locationQuery) {
+  // Canned Responses:
   const coldResponse =
     "It seems a little cold for a traditional Glizzy. Perhaps some Mac'n'Glizzies is in order?";
   const hotResponse = "OH YOU BETTER BELIEVE THATS PRIME GLIZZY TIME";
   const wetResponse = "Rain or shine - IT'S GLIZZY TIME";
   const nightResponse =
     "It's a little late, champ. Get some rest - there will be plenty of time for Glizzies tomorrow.";
+
+  //API call:
   let response;
-  if (window.location.pathname.endsWith("HereBeGlizzies.html")) {
-    response = await getWeather();
-  } else {
+  try {
+    response = await getWeather(locationQuery);
+  } catch (error) {
+    document.getElementById("error").style.display = "block";
     return;
   }
 
-  document.getElementById(
-    "location"
-  ).innerText = `${response.location.name}` //${response.location.country}`;
+  // Show Hidden Elements:
+  document.getElementById("youin").style.display = "block";
+  document.getElementById("glizzyLand").style.display = "block";
+
+  // Populate Elements with Response Data:
+  document.getElementById("location").innerText = `${response.location.name}`;
   document.getElementById(
     "glizzySituation"
   ).innerText = `It's ${response.current.temp_f} degrees and ${response.current.condition.text}.`;
-  let glizzyResponse = document.getElementById("glizzyResponse");
 
+  // Canned Response Logic
   if (response.current.temp_f < 60) {
     glizzyResponse.innerText = coldResponse;
   } else {
     glizzyResponse.innerText = hotResponse;
   }
   if (response.current.condition.text.includes("rain" || "mist")) {
-    glizzyResponse.innerText += wetResponse;
+    glizzyResponse.innerText = wetResponse;
   }
   if (
     response.current.is_day === 1 &&
