182a5d4913d55f0675f511762d2cd11e10d4202b
Michael <mherman315@gmail.com>
2023-12-05 12:12:32 -0800

    Linked printify store lazily

---
+++ js/printify.js
@@ -0,0 +1,11 @@
+'use strict'
+
+// let iframe = document.getElementById('fitPrintIframe');
+// iframe.onload = function() {
+//     let innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+    
+//     let products = innerDoc.getElementsByClassName('product');
+//     for (let i = 0; i < products.length; i++) {
+//         products[i].style.padding = '10px'; // or any padding value you prefer
+//     }
+// };
